06b83c86d4b14f33da65da486b8dfa2006fa396a
BionicWeb <66001029+BionicWeb@users.noreply.github.com>
2020-07-21 08:54:00 +0200

    Create axpertpi.sh

---
+++ axpertpi.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+SUDO=
+if [ "$UID" != "0" ]; then
+	if [ -e /usr/bin/sudo -o -e /bin/sudo ]; then
+		SUDO=sudo
+	else
+		echo '*** This quick installer script requires root privileges.'
+		exit 0
+	fi
+fi
+
+$SUDO apt update
+$SUDO apt install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | $SUDO apt-key add -
+
+echo "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+     $(lsb_release -cs) stable" | \
+    $SUDO tee /etc/apt/sources.list.d/docker.list
+    
+$SUDO apt update
+  
+$SUDO apt install -y --no-install-recommends \
+    docker-ce \
+    cgroupfs-mount
+    
+$SUDO systemctl enable docker
+$SUDO systemctl start docker
+$SUDO usermod -a -G docker $USER
+$SUDO apt install -y python3-pip libffi-dev
+
+$SUDO pip3 install docker-compose
+
+$SUDO git clone https://github.com/ned-kelly/docker-voltronic-homeassistant.git /opt/ha-inverter-mqtt-agent
+cd /opt/ha-inverter-mqtt-agent
+
+wget https://raw.githubusercontent.com/BionicWeb/AxpertPi/master/docker-compose.yml
